5a46f10e8d98875aba18b0b0437ddf1a6778d3e9
Dibyanshu Jaiswal <uname2334@gmail.com>
2022-05-04 16:38:29 +0530

    task 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [image, setImage] = useState('');
+
+    const handleChange = (e) => {
+        let file = e.target.files[0];
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            setImage(e.target.result);
+        };
+        reader.readAsDataURL(file);
+    };
+
+    return (
+        <div className="App">
+            <input id='file' type='file' accept='image/*' onChange={handleChange} />
+            <label htmlFor='file'>
+                <span className='material-symbols-outlined'>
+                    add_photo_alternate
+                </span> &nbsp;
+                Choose a Photo
+            </label>
+            <div style={{ border: '1px dashed black' }}>
+                <TransformWrapper defaultScale={1}>
+                    <TransformComponent>
+                        <div className='image-container'>
+                            <img className='image' src={image} />
+                        </div>
+                    </TransformComponent>
+                </TransformWrapper>
+            </div>
+        </div>
+    );
 }
 
 export default App;
